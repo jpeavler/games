06b53b98a2d970ab8edd3494294a5ed4daef2da3
Joshua Peavler <joshuaapeavler@gmail.com>
2020-09-04 13:31:47 -0600

    Setup router for video games api

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const videoGamesRouter = require('./routes/api/video-games');
 
 const app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/videogames', videoGamesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
